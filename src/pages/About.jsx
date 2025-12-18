e3f9811b1e7bbae70b30c03722430892735d43d2
Sol4rOnGit <Hiresh.Solanki@outlook.com>
2025-12-18 19:59:36 +0000

    Formatting fix [urgent]
    Adjusted the title so that it can be visible. Also fixed the strange issue of 100vw creating a horizontal scrollbar but not 99vw.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -9,21 +9,20 @@ function About(){
             <Header />
             <div className={styles.main}>
                 <div className={styles.section}>
-                    <h1>About us</h1>
+                    <h1 className={styles.title}>About us</h1>
                     <p>The UCGS Computing Society is a student-led community run by experiened sixth form students, open for everyone interested in computer science, technology and problem solving and programming.</p>
                     <p>We aim to create a welcoming place for people to <strong>learn, build, collaborate and network</strong> with their peers, regardless of previous experience.</p>
                     <p>Leaders of the society 2025: Hiresh Solanki, Aryan Jha</p>
                 </div>
 
                 <div className={styles.section}>
-                    <h2>About the Website | Support</h2>
-                    <p className={styles.nomargin}>Created in 2025 by Hiresh to support the Computing Society to help provide a clean, accessible starting point for students who have come for the first time, and to ease them towards more difficult problems. Also to help with management of a variety of skill levels available and mainly</p>
-                    <p className={styles.nomargin}><strong> to eliminate the phrase "I don't know what to do"</strong></p>
+                    <h2>The aim of the website</h2>
+                    <p className={styles.nomargin}>Created in 2025 by Hiresh to support the Computing Society to help provide a clean, accessible starting point for students who have come for the first time, and to ease them towards more difficult problems. Also to help with management of a variety of skill levels available and mainly <strong> to eliminate the phrase "I don't know what to do"</strong></p>
                 </div>
 
                 <div className={styles.section}>
                     <h2>Support and Maintainance</h2>
-                    <p>The website is currenlty Maintained by Hiresh</p>
+                    <p>The website is currenlty maintained by Hiresh</p>
                     <p>For any issues or support, please email <a href="mailto:20solankih@uptoncourtgrammar.org.uk
                             ?subject=Computing%20Society%20Support
                             &body=Please%20describe%20the%20issue%20:)"
