36be5e32b956d3f0a277108862d405ba3466866e
Sol4rOnGit <Hiresh.Solanki@outlook.com>
2025-12-20 11:15:50 +0000

    Get Started partial
    Get started

---
+++ src/pages/GetStarted.jsx
@@ -0,0 +1,18 @@
+import styles from './GetStarted.module.css';
+
+function GetStarted(){
+    return(
+        <div className={styles.main}>
+            <h1>Get Started</h1>
+
+            <div>
+                <h3>What can you do for us?</h3>
+                <p>Hhahfjah</p>
+            </div>
+
+            <h3></h3>
+        </div>
+    )
+}
+
+export default GetStarted
